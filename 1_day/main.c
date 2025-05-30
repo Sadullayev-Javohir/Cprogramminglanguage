ef6000ec616bc382f092f42985a48c3fb013640c
Javohir <javohirsadullayev2024@gmail.com>
2025-05-30 17:37:33 +0500

    1 day

---
+++ 1_day/main.c
@@ -0,0 +1,115 @@
+// ✅ Misol 1: Oddiy konstantani e’lon qilish
+
+// #include <stdio.h>
+// #define PI 3.14
+
+// int main() {
+//   float radius = 5;
+//   float yuza = PI * radius * radius;
+//   printf("Doiraning yuzi: %.2f\n", yuza);
+// }
+
+
+// ✅ Misol 2: Integer konstantani e’lon qilish
+// #include <stdio.h>
+// #define MAX_AGE 100
+
+// int main() {
+//   printf("Eng katta yosh %d\n", MAX_AGE);
+//   return 0;
+// }
+
+
+// ✅ Misol 3: Ifoda (formula) bilan define
+
+// #include <stdio.h>
+// #define KVADRAT(x) ((x) * (x))
+
+// int main() {
+//   int son = 6;
+//   printf("%d sonning kvadrati = %d\n", son, KVADRAT(son));
+//   return 0;
+// }
+
+
+// ✅ Misol 4: Matnli (string) define
+
+// #include <stdio.h>
+// #define SALOM "Assalomu Alaykum"
+
+// int main() {
+//   printf("Matn = %s", SALOM);
+// }
+
+// ✅ Misol 5: Shartli ifodalar bilan #define
+
+// #include <stdio.h>
+// #define MARSMI 1
+
+// int main() {
+//   #if MARSMI
+//   printf("Ha, bu Mars!\n");
+//   #else
+//   printf("Yo'q, bu Yer. \n");
+//   #endif
+//   return 0;
+// }
+
+// ==============================
+
+// Misol 1: Ikki butun sonning yig‘indisini hisoblash
+
+// #include <stdio.h>
+
+// int main() {
+//   int a = 5;
+//   int b = 7;
+//   int yigindi = a + b;
+//   printf("%d", yigindi);
+// }
+
+
+// Misol 2: Haqiqiy sonni o‘zgaruvchiga saqlash va chiqarish
+
+// #include <stdio.h>
+
+// int main() {
+//   float pi = 3.14;
+//   printf("PI ning qiymati: %.2f\n", pi);
+//   return 0;
+// }
+
+
+// Misol 3: Belgini (char) o‘zgaruvchiga saqlash
+
+// #include <stdio.h>
+// int main() {
+//   char harf = 'A';
+//   printf("%c\n", harf);
+//   return 0;
+// }
+
+
+// Misol 4: O‘zgaruvchilarni qiymat bilan e’lon qilish va ularni yangilash
+
+// #include <stdio.h>
+
+// int main() {
+//   int son = 10;
+//   printf("Dastlabki son: %d\n", son);
+
+//   son = 20;
+//   printf("Yangilangan son: %d\n", son);
+//   return 0;
+// }
+
+
+// Misol 5: Konstantani e’lon qilish (const)
+
+// #include <stdio.h>
+
+// int main() {
+//   const float pi = 3.14;
+//   printf("%.2f", pi);
+// }
+
