f5e243ab88a6651baf1394c0b95f706424542b0b
Javohir <javohirsadullayev2024@gmail.com>
2025-06-10 15:14:35 +0500

    9_day

---
+++ 9_day/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  // int n;
+  // printf("Massiv uzunligini kiriting: ");
+  // scanf("%d", &n);
+
+  // int arr[n];
+
+  // printf("Massivlarni kiriting: \n");
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("[%d] = ", i);
+  //   scanf("%d", &arr[i]);
+  // }
+
+  // // 2 baravarga oshirish
+
+  // int *p = arr;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   *(p + i) = *(p + i) * 2;
+  // }
+
+  // printf("Natijasi: \n");
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("[%d] = %d\n", i, arr[i]);
+  // }
+}
