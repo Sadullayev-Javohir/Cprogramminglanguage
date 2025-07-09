d27c6dce97cb15168b69b6040c982ecda475e3ee
Javohir <javohirsadullayev2024@gmail.com>
2025-07-09 23:14:00 +0500

    35_day

---
+++ 35_day/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// int main()
+// {
+//   int a = 3;
+//   int b = 5;
+
+//   printf("a & b %d\n", a & b);
+//   printf("a | b = %d\n", a | b);
+//   printf("a ^ b = %d\n", a ^ b);
+//   printf("a ~ b = %d\n", ~a);
+//   printf("a << 1 = %d\n", a << 1);
+//   printf("a >> 1 = %d\n", a >> 1);
+// }
+
+
+// #include <stdio.h>
+
+// struct Flags
+// {
+//   unsigned int isOn : 1;
+//   unsigned int hasData: 1;
+//   unsigned int error: 1;
+// };
+
+// int main()
+// {
+//   struct Flags f = {1, 0, 1};
+//   printf("isOn: %u\n", f.isOn);
+//   printf("hasData: %u\n", f.hasData);
+//   printf("error: %u\n", f.error);
+//   return 0;
+// }
+
+// int main()
+// {
+//   int x = 5;
+//   int n = 3;
+
+//   x = x | (1 << n);
+
+//   printf("%d",1 << 3);
+// }
