5f767c54b88b84116623e516b2336a5e9622aa6d
Javohir <javohirsadullayev2024@gmail.com>
2025-07-11 20:50:01 +0500

    36_day

---
+++ 36_day/main.c
@@ -0,0 +1,15 @@
+#include "math_module.h"
+#include <stdio.h>
+
+int main()
+{
+  int a = 15, b = 52;
+
+  printf("Add: %d", a + b);
+  printf("Divide: %d", a / b);
+  printf("Subtract: %d", a - b);
+  printf("Multiply: %d", a * b);
+}
+
+
+
---
+++ 36_day/math_module.c
@@ -0,0 +1,23 @@
+#include "math_module.h"
+
+int add(int a, int b)
+{
+  return a + b;
+}
+
+int subtract(int a, int b)
+{
+  return a - b;
+}
+
+int multiply(int a, int b)
+{
+  return a * b;
+}
+
+double divide(int a, int b)
+{
+  return a / b;
+}
+
+
